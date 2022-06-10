1971f68e9d4746c66da5b776bf6916f535fad1c2
vladmog <vladmog@gmail.com>
2022-06-09 22:28:51 -0700

    added starter template

---
+++ p5/starter/sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(500,500);
+  strokeWeight(1);
+  stroke(0);
+  noFill();
+  angleMode(DEGREES);
+  frameRate(30);
+}
+
+
+
+function draw() {
+ 
+  background(200);
+  line(30,30,300,300)
+
+  noLoop(); 
+}
+
+
+
