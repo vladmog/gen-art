c63d6adb213bb618adb9b54ae6732b7914041fe9
vladmog <vladmog@gmail.com>
2022-06-22 19:10:21 -0700

    loaded a custom font

---
+++ p5/projects/textManipulation/sketch.js
@@ -0,0 +1,38 @@
+// =========== S E T U P ================
+
+// Enter desired plot size in inches
+const plotW = 4;
+const plotH = 4;
+const scale = 1;
+dim.updateDims(plotW, plotH, scale);
+
+// Derive stroke weight
+const defPen = "Pilot G2 0.7";
+const defStrokeWeight = dim.inchToPx(lineWidth(defPen));
+
+// ======== E N D   S E T U P ============
+
+let font;
+function preload() {
+	font = loadFont("assets/Montauk-8MBd2.otf");
+}
+
+function setup() {
+	createCanvas(dim.pxDimensions.x, dim.pxDimensions.y, SVG);
+	strokeWeight(defStrokeWeight);
+	stroke(0);
+	noFill();
+	angleMode(DEGREES);
+	frameRate(30);
+}
+
+function draw() {
+	line(0, 0, width, height);
+
+	textFont(font);
+	textSize(200);
+	text("hello", width / 2, height / 2);
+
+	noLoop();
+	// save("mySVG.svg")
+}
--- references.md
+++ references.md
@@ -1,4 +1,7 @@
-# Some references that are not on p5 references page for some reason
+# Some references that are not on p5 references page or I want to try out
 
 textToPoints
 https://p5js.org/reference/#/p5.Font/textToPoints
+
+strokeCap
+https://p5js.org/reference/#/p5/strokeCap
