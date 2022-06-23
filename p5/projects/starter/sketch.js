c6284b953d992f0cf0c0823b1f6cf1518237328e
vladmog <vladmog@gmail.com>
2022-06-22 18:44:33 -0700

    abstracted canvas scaling into a function in dims

--- p5/projects/starter/sketch.js
+++ p5/projects/starter/sketch.js
@@ -1,30 +1,14 @@
 // =========== S E T U P ================
+
 // Enter desired plot size in inches
-const wInches = 4;
-const hInches = 4;
-
-// Enter pen
-const defaultPen = "Pilot G2 0.7";
-
-// Derive canvas size equivalent to plot size
-dim.inchDimensions = {
-	x: (dim.inchDimensions.x *= wInches),
-	y: (dim.inchDimensions.y *= hInches),
-};
-dim.pxDimensions = {
-	x: (dim.pxDimensions.x *= wInches),
-	y: (dim.pxDimensions.y *= hInches),
-};
-
-// Scale canvas (optional. useful if plot is bigger than screen)
-const scaleFactor = 1; // 1 = no scaling applied.
-dim.pxDimensions = {
-	x: (dim.pxDimensions.x *= scaleFactor),
-	y: (dim.pxDimensions.y *= scaleFactor),
-};
+const plotW = 4;
+const plotH = 4;
+const scale = 1;
+dim.updateDims(plotW, plotH, scale);
 
 // Derive stroke weight
-const defStrokeWeight = dim.inchToPx(lineWidth(defaultPen));
+const defPen = "Pilot G2 0.7";
+const defStrokeWeight = dim.inchToPx(lineWidth(defPen));
 
 // ======== E N D   S E T U P ============
 
--- p5/utils/dimConversions/dimConversions.js
+++ p5/utils/dimConversions/dimConversions.js
@@ -5,15 +5,58 @@ aka 3 inches = 391 px
 
 const dim = {};
 
+// ::::::: D I M E N S I O N A L   S T A T E :::::::::::
+
+// These will be modified in every instance of a sketch
+// Default represents mapping of 1 inch to 130.3 px
+
+// Dimensions of plot in inches
 dim.inchDimensions = {
 	x: 1,
 	y: 1,
 };
 
+// Dimensions of sketch in pixels
 dim.pxDimensions = {
 	x: 130.3,
 	y: 130.3,
 };
+
+dim.updateDims = (w, h, scale = 1) => {
+	/* 
+
+	Updates inchDimensions and pxDimensions to represent 
+	dimensions specific to sketch calling the function
+
+	w : Num 
+	width of plot in inches
+
+	h : Num 
+	height of plot in inches
+
+	scale : Num 
+	Scales canvas.
+	By default scale is 1, for a 1:1 scale canvas:plot
+
+	*/
+
+	// Derive canvas size equivalent to plot size
+	dim.inchDimensions = {
+		x: (dim.inchDimensions.x *= w),
+		y: (dim.inchDimensions.y *= h),
+	};
+	dim.pxDimensions = {
+		x: (dim.pxDimensions.x *= w),
+		y: (dim.pxDimensions.y *= h),
+	};
+
+	dim.pxDimensions = {
+		x: (dim.pxDimensions.x *= scale),
+		y: (dim.pxDimensions.y *= scale),
+	};
+};
+
+// ::::   C O N V E R S I O N   H E L P E R   F U N C T I O N S  :::::
 
 dim.inchToPx = inches => {
 	const px = (dim.pxDimensions.x * inches) / dim.inchDimensions.x;
